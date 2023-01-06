17176183e4bf74417487659f17da292e7d9cb861
rwlc <yshsbhat@outlook.com>
2023-01-06 20:42:53 +0530

    Fixed what was broken.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ mod walker;
 
 
 
-#[tokio::main]
-async fn main() {
+
+fn main() {
     let args = walker::Args {
-        url: "https://ynb.sh".to_string()
+        url: "https://ynb.sh".to_string(),
     };
 
 
-    let links = args.recursively_get_links_from_website(None).await;
+    let links = args.recursively_get_links_from_website(None);
 
     println!("{:#?}", links)
 }
--- src/walker.rs
+++ src/walker.rs
@@ -1,4 +1,6 @@
 use scraper::{Html, Selector};
+use std::collections::HashMap;
+use url::Url;
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -8,24 +10,29 @@ pub struct Args {
 
 #[derive(Debug)]
 pub struct Urls {
-    urls: Vec<String>,
+    pub urls: Vec<String>,
+    pub map: HashMap<String, i32>,
 }
 
 impl Urls {
     fn push(&mut self, value: String) {
         self.urls.push(value)
     }
+
+    fn add_map(&mut self, value: String) -> Option<i32> {
+        self.map.insert(value, 1)
+    }
 }
 
 impl Args {
-    pub async fn get_html(&self, url: String) -> Result<String, Box<dyn std::error::Error>> {
-        let res = reqwest::get(url).await?.text().await?;
+    pub fn get_html(&self, url: String) -> Result<String, Box<dyn std::error::Error>> {
+        let res = reqwest::blocking::get(url)?.text()?;
 
         Ok(res)
     }
 
-    pub async fn parse_html(&self, nested_url: String) -> Html {
-        let html = self.get_html(nested_url).await.unwrap();
+    pub fn parse_html(&self, nested_url: String) -> Html {
+        let html = self.get_html(nested_url).unwrap();
         let document = Html::parse_document(html.as_str());
 
         return document;
@@ -39,7 +46,22 @@ impl Args {
         url.starts_with("/")
     }
 
-    pub async fn recursively_get_links_from_website(&self, url: Option<String>) -> Urls {
+    pub fn base_url(&self, mut url: Url) -> Result<Url, &str> {
+        match url.path_segments_mut() {
+            Ok(mut path) => {
+                path.clear();
+            }
+            Err(_) => {
+                return Err("Cannot base URL");
+            }
+        }
+
+        url.set_query(None);
+
+        Ok(url)
+    }
+
+    pub fn recursively_get_links_from_website(&self, url: Option<String>) -> Urls {
         let effective_url = match url {
             Some(k) => k,
             None => {
@@ -49,12 +71,15 @@ impl Args {
             }
         };
 
-        let document = self.parse_html(effective_url).await;
+        let document = self.parse_html(effective_url);
 
         let a_tags_selector = self.get_tag_by_name("a");
         let a_tags = document.select(&a_tags_selector);
 
-        let mut urls = Urls { urls: vec![] };
+        let mut urls = Urls {
+            urls: vec![],
+            map: HashMap::new(),
+        };
 
         for tag in a_tags {
             let value = tag.value();
@@ -64,10 +89,7 @@ impl Args {
             };
 
             if href != "" {
-                let href_clone = href.clone();
-                if self.is_relative_url(href_clone) {
-                    println!("Relative url spotted {}", href)
-                }
+                urls.add_map(href.clone());
                 urls.push(href)
             }
         }
