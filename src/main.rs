5a3bd06c2e04eb00f5f216431d6e1ed42e54e55a
rwlc <yshsbhat@outlook.com>
2023-01-10 13:52:15 +0530

    Improvements

--- src/main.rs
+++ src/main.rs
@@ -138,12 +138,12 @@ fn store_output(set: HashSet<String>, url: String) -> std::io::Result<String> {
             let cl = working_dir.clone() + &save_path;
             let links_cl = links.clone();
 
-            match write(cl.clone() , links_cl) {
+            match write(cl.clone(), links_cl) {
                 Ok(n) => n,
                 Err(e) => println!("Some error occurred: {}", e),
-            } 
+            }
             return Ok(format!("Saved to {cl}"));
-        },
+        }
     }
 
     let cl = save_path.clone();
@@ -159,6 +159,7 @@ fn store_output(set: HashSet<String>, url: String) -> std::io::Result<String> {
     }
 
     let success_message = format!("Saved to {}", cl);
+    println!("{}", success_message);
     Ok(success_message)
 }
 
