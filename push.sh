9bacf784adbeadc6eddb7cc8b950d7f592d10e19
rwlc <yshsbhat@outlook.com>
2023-01-18 23:33:22 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,8 +0,0 @@
- cargo build --target=x86_64-pc-windows-gnu --release && 
- cp target/x86_64-pc-windows-gnu/release/walker.exe download/windows/walker.exe &&
- echo "Saved to download/windows" &&
- cargo build -r && 
- cp target/release/walker download/macOS/walker &&
- echo "Saved to download/macOS" &&
- gth --input "README.md" --output "index.html" --title "Walker" -w -b && 
- commit -m "add"
